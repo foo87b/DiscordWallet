5a9663973c39dbe39580cc236d18d87f6e59c6b8
foo87b <foo87b@users.noreply.github.com>
2017-12-25 19:54:42 +0900

    XPネットワークの定義を追加

--- Services/XPWallet.cs
+++ Services/XPWallet.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Discord.WebSocket;
+using NBitcoin;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,21 @@ namespace DiscordWallet.Services
 {
     public class XPWallet
     {
+        public static Network Network { get; private set; }
+        
+        public XPWallet(DiscordSocketClient discord)
+        {
+            Network = new NetworkBuilder()
+                .SetName("xp")
+                .SetMagic(0xe5e2f8b4)
+                .SetPort(28192)
+                .SetRPCPort(28191)
+                .SetBase58Bytes(Base58Type.SECRET_KEY, new byte[] { 203 })
+                .SetBase58Bytes(Base58Type.PUBKEY_ADDRESS, new byte[] { 75 })
+                .SetBase58Bytes(Base58Type.SCRIPT_ADDRESS, new byte[] { 20 })
+                .SetGenesis(new Block()) // FIXME
+                .SetConsensus(new Consensus()) // FIXME
+                .BuildAndRegister();
+        }
     }
 }
